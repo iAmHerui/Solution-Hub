e0272e4d8351e1a37d4937ead08ab3f8ba5e1d9b
iAmHerui <he.rui@h3c.com>
2021-07-20 20:41:12 +0800

    update by herui 2021-07-20:nodeDeploy

--- src/main/java/com/h3c/solutionhub/controller/NodesManagementController.java
+++ src/main/java/com/h3c/solutionhub/controller/NodesManagementController.java
@@ -1,5 +1,8 @@
 package com.h3c.solutionhub.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.h3c.solutionhub.entity.DhcpBO;
 import com.h3c.solutionhub.entity.NodeBo;
 import com.h3c.solutionhub.service.NodesManagementService;
@@ -9,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Api(value = "节点管理",tags = "节点管理")
 @RestController
@@ -75,22 +80,23 @@ public class NodesManagementController {
     @ApiOperation(value = "节点部署",notes = "节点部署")
     @CrossOrigin(origins ="*",maxAge =3600)
     @PostMapping(value = "/nodeDeploy")
-    public Boolean nodeDeploy(
-//            String productType,
-//            String productVersion,
-//            List<NodeBo> nodes) {
-        /** test **/
-        ) {
-        String productType = "CAS";
-        String productVersion = "E0701";
+    public Boolean nodeDeploy(@RequestBody Map<String,Object> params) {
+        String productType = params.get("productType").toString();
+        String productVersion = params.get("productVersion").toString();
+        String nodeListString = params.get("nodes").toString();
 
+        JSONArray jsonArray = JSONArray.parseArray(nodeListString);
         List<NodeBo> nodes = new ArrayList<>();
-        NodeBo node = new NodeBo();
-        node.setManagementIP("170.0.0.36");
-        node.setNodeName("cas-node1");
-        nodes.add(node);
-        /** test **/
+        for(int i=0;i<jsonArray.size();i++) {
+            JSONObject nodeBo = jsonArray.getJSONObject(i);
+            NodeBo node = new NodeBo();
+            node.setManagementIP(nodeBo.getString("managementIP"));
+            node.setNodeName(nodeBo.getString("nodeName"));
+            nodes.add(node);
+        }
+
         return nodesManagementService.deployNode(productType,productVersion,nodes);
+
     }
     
     @ApiOperation(value = "查看DHCP地址",notes = "查看DHCP地址")
